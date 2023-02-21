f6020767b8813d8fc36d0b67af0245d8368dfa1f
marshpin <marshpinvr@gmail.com>
2023-02-21 11:47:40 -0600

    v1.1

--- projects/pygame.MessingAround/gameObjectClasses.py
+++ projects/pygame.MessingAround/gameObjectClasses.py
@@ -4,8 +4,10 @@ import sys
 from colorama import Fore
 from sys import exit as sysexit
 
+
 layers = ["background", "ground", "gfx"]
 
+gameObjects = {}
 
 def ErrorMessage(msg, quit):
     print(Fore.RED + "ERROR: " + msg)
@@ -20,10 +22,21 @@ class Rectangle(pygame.Rect):
         self.height = height
         self.color = color
         self.objname = objname
+        self.rect = pygame.Rect(self.x,self.y,self.width,self.height)
         if not layer in layers:
             ErrorMessage(f"Layer type of {self.objname} is unrecognizable! Unrecognized layer: {layer}", True)
         else:
             self.layer = layer
+        gameObjects[self.layer].append((self.objname, self.rect, self.color))
 class GameObjectHandler():
     def __init__(self):
-        self.GameObjects = []
+        self.GameObjects = gameObjects
+        for layer in layers:
+            gameObjects[layer] = []
+    def update(self, screen):
+        self.GameObjects = gameObjects
+        for layer in self.GameObjects:
+            for obj in self.GameObjects[layer]:
+                rect = obj[1]
+                color = obj[2] 
+                pygame.draw.rect(screen, color, rect)
--- projects/pygame.MessingAround/main.py
+++ projects/pygame.MessingAround/main.py
@@ -7,7 +7,7 @@ from sys import exit as sysexit
 from pygame import QUIT, quit as pyquit
 from player import Player
 from colorama import init as colorinit
-from gameObjectClasses import Rectangle
+from gameObjectClasses import Rectangle, GameObjectHandler
 colorinit()
 
 gameLoop = True
@@ -19,14 +19,21 @@ pygame.display.set_caption("Pygame home Project")
 clock = pygame.time.Clock()
 
 player = Player(16,16,(255,255,255))
+ObjectHandler = GameObjectHandler()
+
+Rectangle(0,0,screenSize[0], screenSize[1], (12,12,12), "background", "backgroundblock")
+Rectangle(50,50,28,28,(0,0,255), "background", "rect1")
+Rectangle(128,128,32,32,(255,0,255), "background", "rect2")
+Rectangle(200,200,16,16,(255,0,0), "background", "rect3")
+Rectangle(100,150,64,64,(0,255,0), "background", "rect4")
+Rectangle(0, screenSize[1] - 70, screenSize[0], 70, (24,24,24), "ground", "groundblock")
 
 while gameLoop:
     for event in pygame.event.get():
         if event.type == QUIT:
             pyquit()
             sysexit()
-    screen.fill((0,0,0))
-    rect1 = Rectangle(100,150,64,64,(255,0,0), "background", "rect1")
-    player.update(screen)
+    ObjectHandler.update(screen)
+    player.update(screen, ObjectHandler.GameObjects)
     pygame.display.update()
     clock.tick(60)
