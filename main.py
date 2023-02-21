1599980a48b3f4dc1955eeeaaaa02d814b95ada3
marshpin <marshpinvr@gmail.com>
2023-02-21 15:04:05 -0600

    v.02

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import pygame
+
+gameLoop = True
+
+screenSize = (400,400)
+screen = pygame.display.set_mode(screenSize)
+pygame.display.set_caption("Pygame home Project")
+
+if gameLoop:
+    pygame.display.update()
