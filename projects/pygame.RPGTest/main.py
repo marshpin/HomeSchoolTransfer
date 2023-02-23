f9ad1f7c6959c38abe227b8579f0ed2d056b9815
marshpin <marshpinvr@gmail.com>
2023-02-23 12:59:33 -0600

    v8

---
+++ projects/pygame.RPGTest/main.py
@@ -0,0 +1,8 @@
+import pygame 
+import pillow as pil
+from pygame import QUIT, KEYDOWN, rect, Rect
+
+screenSize = (400,400)
+screen = pygame.display.set_mode(screenSize)
+pygame.display.set_caption("Game Window")
+pygame.display.set_icon()
